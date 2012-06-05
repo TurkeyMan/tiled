b0314e26b34acb0cf49de6db035d38abac349338
Manu Evans <turkeyman@gmail.com>
2012-06-05 22:22:36 +0300

    Added ice cave example
    This example shows how the terrain system can be used to paint false-perspective walls.
    Again, there are a couple of tiles missing which shows some errors.

---
+++ examples/icecave.tsx
@@ -0,0 +1,77 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="icecave" tilewidth="32" tileheight="32">
+ <image source="icecave.png" width="512" height="256"/>
+ <terraintypes>
+  <terrain name="Ice" tile="4" distances=",0,1,1,,,"/>
+  <terrain name="Sand" tile="110" distances=",1,0,2,,,"/>
+  <terrain name="Floor" tile="27" distances=",1,2,0,,,"/>
+  <terrain name="Wall-Low" tile="68" distances="1,,,,0,1,1"/>
+  <terrain name="Wall-High" tile="52" distances="2,,,,1,0,1"/>
+  <terrain name="Darkness" tile="20" distances="3,,,,1,1,0"/>
+ </terraintypes>
+ <tile id="1" terrain="0,0,0,0" probability="3"/>
+ <tile id="2" terrain="0,0,0,0" probability="3"/>
+ <tile id="3" terrain=",5,,5"/>
+ <tile id="4" terrain="0,0,0,0"/>
+ <tile id="5" terrain="5,,5,"/>
+ <tile id="8" terrain="5,,5,5"/>
+ <tile id="9" terrain=",5,5,5"/>
+ <tile id="10" terrain="0,0,0,2"/>
+ <tile id="11" terrain="0,0,2,2"/>
+ <tile id="12" terrain="0,0,2,0"/>
+ <tile id="17" terrain="5,5,4,5"/>
+ <tile id="18" terrain="5,5,5,4"/>
+ <tile id="19" terrain=",5,,5"/>
+ <tile id="20" terrain="5,5,5,5"/>
+ <tile id="21" terrain="5,,5,"/>
+ <tile id="22" terrain=",,,5"/>
+ <tile id="23" terrain=",,5,5"/>
+ <tile id="24" terrain=",,5,"/>
+ <tile id="26" terrain="0,2,0,2"/>
+ <tile id="27" terrain="2,2,2,2"/>
+ <tile id="28" terrain="2,0,2,0"/>
+ <tile id="33" terrain="4,5,3,4"/>
+ <tile id="34" terrain="5,4,4,3"/>
+ <tile id="35" terrain=",5,,4"/>
+ <tile id="36" terrain="5,5,4,4"/>
+ <tile id="37" terrain="5,,4,"/>
+ <tile id="38" terrain="5,5,5,4"/>
+ <tile id="39" terrain="5,5,4,4"/>
+ <tile id="40" terrain="5,5,4,5"/>
+ <tile id="42" terrain="0,2,0,0"/>
+ <tile id="43" terrain="2,2,0,0"/>
+ <tile id="44" terrain="2,0,0,0"/>
+ <tile id="49" terrain="3,4,,3"/>
+ <tile id="50" terrain="4,3,3,"/>
+ <tile id="51" terrain=",4,,3"/>
+ <tile id="52" terrain="4,4,3,3"/>
+ <tile id="53" terrain="4,,3,"/>
+ <tile id="54" terrain="5,4,5,3"/>
+ <tile id="55" terrain="4,4,3,3"/>
+ <tile id="56" terrain="4,5,3,5"/>
+ <tile id="59" terrain="2,2,2,0"/>
+ <tile id="60" terrain="2,2,0,2"/>
+ <tile id="65" terrain=",3,,"/>
+ <tile id="66" terrain="3,,,"/>
+ <tile id="67" terrain=",3,,"/>
+ <tile id="68" terrain="3,3,,"/>
+ <tile id="69" terrain="3,,,"/>
+ <tile id="70" terrain="5,3,5,"/>
+ <tile id="71" terrain="3,3,,"/>
+ <tile id="72" terrain="3,5,,5"/>
+ <tile id="75" terrain="2,0,2,2"/>
+ <tile id="76" terrain="0,2,2,2"/>
+ <tile id="91" terrain="1,1,1,0"/>
+ <tile id="92" terrain="1,1,0,1"/>
+ <tile id="93" terrain="0,0,0,1"/>
+ <tile id="94" terrain="0,0,1,1"/>
+ <tile id="95" terrain="0,0,1,0"/>
+ <tile id="107" terrain="1,0,1,1"/>
+ <tile id="108" terrain="0,1,1,1"/>
+ <tile id="109" terrain="0,1,0,1"/>
+ <tile id="110" terrain="1,1,1,1"/>
+ <tile id="111" terrain="1,0,1,0"/>
+ <tile id="125" terrain="0,1,0,0"/>
+ <tile id="126" terrain="1,1,0,0"/>
+ <tile id="127" terrain="1,0,0,0"/>
+</tileset>
