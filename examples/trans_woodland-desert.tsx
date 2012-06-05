2017410a7033a2358d6f250f4e2bbbc8798faefc
Manu Evans <turkeyman@gmail.com>
2012-06-05 20:44:48 +0300

    Added woodland-desert example
    Features 3-way transitions.
    There are lots of transitions missing from this tileset, so it's possible to paint broken intersections, but it's a good example.

---
+++ examples/trans_woodland-desert.tsx
@@ -0,0 +1,96 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="trans_woodland-desert" tilewidth="32" tileheight="32">
+ <image source="trans_woodland-desert.png" width="512" height="256"/>
+ <terraintypes>
+  <terrain name="Grass" tile="0" distances=",0,1,2,2"/>
+  <terrain name="Dirt" tile="18" distances=",1,0,2,1"/>
+  <terrain name="Water" tile="21" distances=",2,2,0,2"/>
+  <terrain name="Desert" tile="30" distances=",2,1,2,0"/>
+ </terraintypes>
+ <tile id="0" terrain="0,0,0,0"/>
+ <tile id="1" terrain="0,0,0,1"/>
+ <tile id="2" terrain="0,0,1,1"/>
+ <tile id="3" terrain="0,0,1,0"/>
+ <tile id="4" terrain="1,1,1,2"/>
+ <tile id="5" terrain="1,1,2,2"/>
+ <tile id="6" terrain="1,1,2,1"/>
+ <tile id="13" terrain="1,1,1,3"/>
+ <tile id="14" terrain="1,1,3,3"/>
+ <tile id="15" terrain="1,1,3,1"/>
+ <tile id="16" terrain="0,0,0,0"/>
+ <tile id="17" terrain="0,1,0,1"/>
+ <tile id="18" terrain="1,1,1,1"/>
+ <tile id="19" terrain="1,0,1,0"/>
+ <tile id="20" terrain="1,2,1,2"/>
+ <tile id="21" terrain="2,2,2,2"/>
+ <tile id="22" terrain="2,1,2,1"/>
+ <tile id="29" terrain="1,3,1,3"/>
+ <tile id="30" terrain="3,3,3,3"/>
+ <tile id="31" terrain="3,1,3,1"/>
+ <tile id="32" terrain="0,0,0,0"/>
+ <tile id="33" terrain="0,1,0,0"/>
+ <tile id="34" terrain="1,1,0,0"/>
+ <tile id="35" terrain="1,0,0,0"/>
+ <tile id="36" terrain="1,2,1,1"/>
+ <tile id="37" terrain="2,2,1,1"/>
+ <tile id="38" terrain="2,1,1,1"/>
+ <tile id="45" terrain="1,3,1,1"/>
+ <tile id="46" terrain="3,3,1,1"/>
+ <tile id="47" terrain="3,1,1,1"/>
+ <tile id="48" terrain="0,0,0,0"/>
+ <tile id="49" terrain="0,2,2,2"/>
+ <tile id="50" terrain="2,0,2,2"/>
+ <tile id="51" terrain="1,1,1,0"/>
+ <tile id="52" terrain="1,1,0,1"/>
+ <tile id="53" terrain="2,2,2,1"/>
+ <tile id="54" terrain="2,2,1,2"/>
+ <tile id="60" terrain="3,3,3,1"/>
+ <tile id="61" terrain="3,3,1,3"/>
+ <tile id="62" terrain="2,2,2,3"/>
+ <tile id="63" terrain="2,2,3,2"/>
+ <tile id="64" terrain="0,0,0,0"/>
+ <tile id="65" terrain="2,2,0,2"/>
+ <tile id="66" terrain="2,2,2,0"/>
+ <tile id="67" terrain="1,0,1,1"/>
+ <tile id="68" terrain="0,1,1,1"/>
+ <tile id="69" terrain="2,1,2,2"/>
+ <tile id="70" terrain="1,2,2,2"/>
+ <tile id="76" terrain="3,1,3,3"/>
+ <tile id="77" terrain="1,3,3,3"/>
+ <tile id="78" terrain="2,3,2,2"/>
+ <tile id="79" terrain="3,2,2,2"/>
+ <tile id="81" terrain="0,0,0,2"/>
+ <tile id="82" terrain="0,0,2,2"/>
+ <tile id="83" terrain="0,0,2,0"/>
+ <tile id="93" terrain="3,3,3,2"/>
+ <tile id="94" terrain="3,3,2,2"/>
+ <tile id="95" terrain="3,3,2,3"/>
+ <tile id="97" terrain="0,2,0,2"/>
+ <tile id="98" terrain="2,2,2,2"/>
+ <tile id="99" terrain="2,0,2,0"/>
+ <tile id="100" terrain="2,1,2,0"/>
+ <tile id="101" terrain="1,2,0,2"/>
+ <tile id="102" terrain="2,2,1,0"/>
+ <tile id="103" terrain="2,2,0,1"/>
+ <tile id="105" terrain="2,1,2,3"/>
+ <tile id="106" terrain="1,2,3,2"/>
+ <tile id="107" terrain="2,2,1,3"/>
+ <tile id="108" terrain="2,2,3,1"/>
+ <tile id="109" terrain="3,2,3,2"/>
+ <tile id="110" terrain="2,2,2,2"/>
+ <tile id="111" terrain="2,3,2,3"/>
+ <tile id="113" terrain="0,2,0,0"/>
+ <tile id="114" terrain="2,2,0,0"/>
+ <tile id="115" terrain="2,0,0,0"/>
+ <tile id="116" terrain="2,0,2,1"/>
+ <tile id="117" terrain="0,2,1,2"/>
+ <tile id="118" terrain="1,0,2,2"/>
+ <tile id="119" terrain="0,1,2,2"/>
+ <tile id="121" terrain="2,3,2,1"/>
+ <tile id="122" terrain="3,2,1,2"/>
+ <tile id="123" terrain="1,3,2,2"/>
+ <tile id="124" terrain="3,1,2,2"/>
+ <tile id="125" terrain="3,2,3,3"/>
+ <tile id="126" terrain="2,2,3,3"/>
+ <tile id="127" terrain="2,3,3,3"/>
+</tileset>
